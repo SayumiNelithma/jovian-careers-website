f6246d7af0e7f8eeeda9ce93a38b6d7da1261f2d
Sayumi <156690349+SayumiNelithma@users.noreply.github.com>
2025-03-13 06:25:41 +0000

    commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from flask.ctx import F
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == '__main__':
