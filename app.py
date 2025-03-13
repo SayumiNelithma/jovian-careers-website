07c0cc2cf9b4ffe72c86ea2a0f4611c418aea5b0
Sayumi <156690349+SayumiNelithma@users.noreply.github.com>
2025-03-13 04:54:15 +0000

    added the flask project

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print ("Hello World!")
+from flask import Flask
+from flask.ctx import F
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+print(__name__)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
